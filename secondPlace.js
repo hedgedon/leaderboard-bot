bcab1991ba64b3d955d0ae70e87f2733da646874
hedgedon <donpark@pm.me>
2022-02-03 15:53:02 -0600

    updates url and fixes leaderboard

--- secondPlace.js
+++ secondPlace.js
@@ -1,24 +1,24 @@
-require("dotenv").config();
-const Discord = require("discord.js");
+require('dotenv').config();
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.BOT2_TOKEN;
-const serverId = process.env.DEV_SERVER_ID;
+const serverId = process.env.SERVER_ID;
 client.login(token);
 
-client.on("ready", () => {
+client.on('ready', () => {
   console.log(
-    ">>> 2nd Place Discord bot is Online, please wait while its fetching data <<<"
+    '>>> 2nd Place Discord bot is Online, please wait while its fetching data <<<'
   );
   setBot();
 });
-client.on("rateLimit", (info) => {
+client.on('rateLimit', (info) => {
   console.log(
     `Rate limit hit ${
       info.timeDifference
         ? info.timeDifference
         : info.timeout
         ? info.timeout
-        : "Unknown timeout "
+        : 'Unknown timeout '
     }`
   );
 });
@@ -30,12 +30,15 @@ async function setBot(poolName2nd, poolPerformance2nd, poolTotalValue2nd) {
     await guild.me.setNickname(`2. ${poolName2nd}`);
 
     // SET POOLS TOKEN VALUE AS PLAYING
-    await client.user.setActivity(`🥈 +${poolPerformance2nd}% | ${poolTotalValue2nd}`, {
-      type: "PLAYING",
-    });
-    console.log("executed setBot() to set the name & activity!");
+    await client.user.setActivity(
+      `🥈 +${poolPerformance2nd}% | ${poolTotalValue2nd}`,
+      {
+        type: 'PLAYING',
+      }
+    );
+    console.log('executed setBot() to set the name & activity!');
   } catch (error) {
-    console.log("Your Error: ", error);
+    console.log('Your Error: ', error);
   }
 }
 exports.setBot = setBot;
